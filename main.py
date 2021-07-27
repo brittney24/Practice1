cd18fd0c4763b74df8e6b18f0a518929ad2fc761
Brittney Koshy <88042497+brittney24@users.noreply.github.com>
2021-07-27 14:11:40 +0000

    total and diner

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #This is a practice Python.
+input1 = int(input("Please enter a number: "))
+input2 = int(input("Please enter a number: "))
+input3 = int(input("Please enter a number: "))
+input4 = int(input("Please enter a number: "))
+
+print("This is your average: " + str(input1 + input2 + input3 + input4) / 4)
+
+total = int(input("What is your total bill?: "))
+diner = int(input("How many diners ate this meal?: "))
+print("This is how much each person should pay: " + str())
