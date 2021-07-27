2738ba819483342e4d1847b347d8ef8c83c26103
Brittney Koshy <88042497+brittney24@users.noreply.github.com>
2021-07-27 14:52:49 +0000

    star + circle

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import turtle
 t = turtle.Turtle()
-for c in ['red', 'green', 'yellow', 'blue', 'purple']:
-  t.color(c)
-  t.forward(9)
-  t.left(45)
-  t.forward(9)
-  t.right(35)
-  t.forward(9)
+for i in range(8):
+  for c in ['red', 'green', 'yellow', 'blue', 'purple']:
+    t.color(c)
+    t.forward(9)
+    t.left(45)
+    t.forward(9)
+    t.right(35)
+    t.forward(9)
+
+
+t.pu()
+t.goto(-75,170)
+t.pd()
+for i in ['red', 'green', 'yellow', 'blue', 'purple']:
+  t.color(i)
+  t.forward(100)
+  t.right(144)
